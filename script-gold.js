91ab85a0a6eceafc43c5dc0b0c2f3f892e23860d
Dennis Kupec <denniskupec@gmail.com>
2015-07-18 20:36:31 -0500

    Update script-gold.js

--- script-gold.js
+++ script-gold.js
@@ -3,7 +3,7 @@
 // @namespace   denniskupec.com
 // @include     https://vulcun.com/user/jackpot
 // @version     1
-// @grant       none
+// @grant				none
 // ==/UserScript==
 
 var $ = unsafeWindow.$;
@@ -11,41 +11,26 @@ var waiting = false;
 
 $("#countdown-timer").bind("change", function()
 {
-  var countdown = parseInt($(this).val());
-  var item_price = parseFloat($("#item-price").text());
-  var current_gold = parseInt($(".vu-fee-filter").attr("max"));
+	var countdown = parseInt($(this).val());
+	var item_price = parseFloat($("#item-price").text());
+	var current_gold = parseInt($(".vu-fee-filter").attr("max"));
+	var odds = parseFloat($("#betting-odds").text());
 
-  if (!waiting && countdown > 0 && current_gold > 1000 && !isNaN(item_price) && item_price > 0)
-  {
-    waiting = true;
-    var new_bet_amount = Math.floor(item_price / 0.00125);
-     
-    if (new_bet_amount > 1000) new_bet_amount -= 180;
+	if (!waiting && countdown > 0 && item_price > 0) {
+		var new_bet_amount = (item_price > 2) ? Math.floor((item_price / 0.00125) + 200) : 0;
+
+		$(".vu-fee-filter").val(new_bet_amount);
+		$("#betting-amount").text(new_bet_amount + " Gold");
+		$("#submit-wager").click();
+		waiting = true;
     
-    $(".vu-fee-filter").val(new_bet_amount);
-    updateJackpotWager(new_bet_amount);
-
-    $("#betting-amount").text(new_bet_amount + " Gold");
-    $("#submit-wager").click();
-   
-    console.log();
+		console.log();
 		console.log("Submitted jackpot: $" + item_price + " for " + new_bet_amount + " gold");
-    console.log("Odds: " + $("#betting-odds").text());
-    console.log("Remaining Gold: " + current_gold);
-    console.log();
-	  console.log("waiting for the site to update...");
-  }
-
-  if (countdown == 0)
-  {
-    var check = setInterval(function(){
-      if (parseInt($("#countdown-timer").val()) > 0)
-      {
-        waiting = false;
-        console.log("website updated!");
-        clearInterval(check);
-      }
-    }, 1000);
-  }
+		console.log("waiting for the site to update...");
+	}
+  
+	if (countdown == 0) {
+		waiting = false;
+	}
 
 });
